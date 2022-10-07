bd2c22b63720d4b79bdf8d75b51744ff8ee9f585
Raitoningu21 <andrei1995tokarew@mail.ru>
2022-10-07 16:30:15 +0300

    PatternIP

--- src/Regular.java
+++ src/Regular.java
@@ -1,10 +1,14 @@
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regular
 {
     public boolean isIp(String str)
     {
-        String patternIp = "";
+        String patternIp = "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b)"+
+                "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b)" + "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b)" +
+                "((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b)";
+
         Pattern pattern = Pattern.compile(patternIp);
         Matcher matcher = pattern.matcher(str);
         return matcher.matches();
